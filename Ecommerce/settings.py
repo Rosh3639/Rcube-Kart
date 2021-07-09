fc4848cb3deee752b3a748d9e8e4e217aac0fe61
Roshan <rshnrasal@gmail.com>
2021-07-09 23:49:26 +0530

    First Commit

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os.path
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,4 +132,5 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+# Activate Django-Heroku.
+django_heroku.settings(locals())
